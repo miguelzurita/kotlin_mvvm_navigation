83df007f5b144e92e325a60cf400fe50ae4e3597
miguel <miguel@development.com>
2021-05-10 00:06:24 -0300

    -f show user feedback on save

--- app/src/main/java/com/miguelzurita/demo/AddFragment.kt
+++ app/src/main/java/com/miguelzurita/demo/AddFragment.kt
@@ -1,17 +1,18 @@
 package com.miguelzurita.demo
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.Observable
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.miguelzurita.demo.databinding.FragmentAddBinding
 import com.miguelzurita.demo.viewmodel.AddViewModel
+
 
 /**
  * A simple [Fragment] subclass.
@@ -34,10 +35,31 @@ class AddFragment : Fragment() {
 
     private fun initializeObserver() {
         viewmodel.saveLiveData.observe(this, Observer {
+            binding.etName.requestFocus()
             if (it) {
-//                show toast success on save
+                // show success feedback on save
+                changeBackgroundColor(resources.getColor(R.color.purple_500), resources.getColor(R.color.teal_200), 300, 1500)
+            } else {
+                // show error feedback
+                changeBackgroundColor(resources.getColor(R.color.purple_500), resources.getColor(R.color.red), 300, 1500)
             }
         })
+    }
+
+    private fun changeBackgroundColor(initColor: Int, temporalColor: Int, animationDuration: Long, duration: Long) {
+        val backgroundColorAnimator = ObjectAnimator.ofObject(binding.btnAdd, "backgroundColor", ArgbEvaluator(), initColor, temporalColor)
+        backgroundColorAnimator.duration = animationDuration
+        backgroundColorAnimator.start()
+
+        object : CountDownTimer(duration, 50) {
+            override fun onTick(arg0: Long) {}
+
+            override fun onFinish() {
+                val backgroundColorAnimator = ObjectAnimator.ofObject(binding.btnAdd, "backgroundColor", ArgbEvaluator(), temporalColor, initColor)
+                backgroundColorAnimator.duration = animationDuration
+                backgroundColorAnimator.start()
+            }
+        }.start()
     }
 
     private fun configureUI() {
