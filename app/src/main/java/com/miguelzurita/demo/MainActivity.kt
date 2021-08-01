bf63f79df5887818b546cfa3c748d1041b14d771
miguel <miguel@development.com>
2021-07-31 22:04:44 -0300

    -f Crashlytics + Firebase Perfomance

--- app/src/main/java/com/miguelzurita/demo/MainActivity.kt
+++ app/src/main/java/com/miguelzurita/demo/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.miguelzurita.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var navigationController: NavController
     lateinit var bottonNav: BottomNavigationView
 
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         bottonNav = bottom_navigation
 
--- app/src/main/java/com/miguelzurita/demo/app/DemoApplication.kt
+++ app/src/main/java/com/miguelzurita/demo/app/DemoApplication.kt
@@ -1,10 +1,13 @@
 package com.miguelzurita.demo.app
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import androidx.room.Room
+import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.miguelzurita.demo.model.room.DeviceDatabase
 
-class DemoApplication : Application() {
+class DemoApplication : MultiDexApplication() {
     companion object {
         lateinit var database: DeviceDatabase
     }
@@ -12,5 +15,8 @@ class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         database = Room.databaseBuilder(this, DeviceDatabase::class.java, "device_database").build()
+
+        FirebaseApp.initializeApp(applicationContext);
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
     }
 }
